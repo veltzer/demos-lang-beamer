9a4562b66c894907c80d3d951c35f78e7a75ec80
Mark Veltzer <mark.veltzer@gmail.com>
2025-02-09 10:31:33 +0200

    no commit message given

--- config/deps.py
+++ config/deps.py
@@ -2,4 +2,5 @@ packages=[
     "texlive-latex-base", # for latex
     "texlive-latex-recommended", # for beamer
     "texlive-pictures", # for pictures
+    "texlive-latex-extra", # for minted.sty
 ]
